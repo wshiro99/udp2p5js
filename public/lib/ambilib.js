9530e93304744e6c2e27c52c8b0f0a2a84c8b377
unknown <wshiro99@gmail.com>
2022-05-13 18:32:19 +0800

    modified floor size setting

--- public/lib/ambilib.js
+++ public/lib/ambilib.js
@@ -1,4 +1,11 @@
 var x=400, y=400
+var xArray=[],yArray=[]
+var tmp
+
+function getNewMouseArray(){
+    var tmpx=xArray
+    return tmpx;
+}
 
 function getNewMouseX(){
     return x;
@@ -6,19 +13,32 @@ function getNewMouseX(){
 function getNewMouseY(){
     return y;
 }
+function getXSpeed(){
+    return tmp[2];
+}
+function getYSpeed(){
+    return tmp[3];
+}
+function getXAcc(){
+    return tmp[4];
+}
+function getYAcc(){
+    return tmp[5];
+}
 
 function getWallInfo(id){
     if(id=="c"){
-        return {xStart:0, xEnd:10, yStart:38, yEnd:30, xMapStart:0, xMapEnd:4782, yMapStart:0, yMapEnd:2160}
+        return {xStart:0, xEnd:10, yStart:38, yEnd:28, xMapStart:0, xMapEnd:4782, yMapStart:0, yMapEnd:2160}
     }
     else if(id=="b"){
-        return {xStart:20, xEnd:28, yStart:0, yEnd:4, xMapStart:0, xMapEnd:13692, yMapStart:0, yMapEnd:2160}
+        return {xStart:27, xEnd:38, yStart:0, yEnd:4, xMapStart:0, xMapEnd:4782, yMapStart:0, yMapEnd:2160}
     }
     else if(id=="d"){
-        return {xStart:28, xEnd:20, yStart:10, yEnd:6, xMapStart:0, xMapEnd:13692, yMapStart:0, yMapEnd:2160}
+        return {xStart:38, xEnd:27, yStart:10, yEnd:6, xMapStart:0, xMapEnd:4782, yMapStart:0, yMapEnd:2160}
     }
     else if(id=="e"){
-        return {xStart:10, xEnd:38, yStart:0, yEnd:10, xMapStart:0, xMapEnd:9128, yMapStart:0, yMapEnd:3188}
+        //return {xStart:38, xEnd:0, yStart:10, yEnd:0, xMapStart:0, xMapEnd:12336, yMapStart:0, yMapEnd:3188}
+        return {xStart:38, xEnd:0, yStart:10, yEnd:0, xMapStart:0, xMapEnd:3084, yMapStart:0, yMapEnd:797}
     }
 }
 
@@ -27,16 +47,21 @@ function setupSocket(wall){
 	var socket = io.connect();		
 	socket.on('message', function(data) {
 		try{
-            var tmp="0:31,10,0,0,0,0;1:30,40,0,0,0,0;2:21,3,0,0,0,0"
-            var hc=tmp.split(";")
+            //clean array
+            while (xArray.length) {
+                xArray.pop();
+            }
+            //var test="0:31,10,0,0,0,0;1:20,4,0,0,0,0;2:5,3,0,0,0,0"
+            //var hc=test.split(";")
+            var hc=data.split(";")
             var idLock=-1
             //var hc=data.toString().split(";") 
             for(var i=0;i<hc.length;i++){
                 var spliter=hc[i].split(":")  
-                var xy=spliter[1].split(",")  //xy[0~5]
+                tmp=spliter[1].split(",")  //tmp[0~5]
                 var id=spliter[0];
-                var mx=xy[0]
-                var my=xy[1]
+                var mx=tmp[0]
+                var my=tmp[1]
                 if(wall=="c"){
                     if(my>=min(o.xStart,o.xEnd)&&my<=max(o.xStart,o.xEnd)&&mx>=min(o.yStart,o.yEnd)&&mx<=max(o.yStart,o.yEnd)){
                         if(idLock<0||idLock==id){
@@ -52,18 +77,29 @@ function setupSocket(wall){
                     }              
                 }
                 else if(wall=="b"||wall=="d"||wall=="e"){
-                    if(mx>=min(o.xStart,o.xEnd)&&mx<=max(o.xStart,o.xEnd)&&my>=min(o.yStart,o.yEnd)&&my<=max(o.yStart,o.yEnd)){
-                        if(idLock<0||idLock==id){
-                            x=lerp(x, map(mx,o.xStart,o.xEnd,o.xMapStart,o.xMapEnd) ,0.3)
-                            y=lerp(y, map(my,o.yStart,o.yEnd,o.yMapStart,o.yMapEnd) ,0.3)	
-                            console.log("id:"+id+","+x+","+y)
-                            idLock=id
+                    if(mx>=min(o.xStart,o.xEnd)&&mx<=max(o.xStart,o.xEnd)&&my>=min(o.yStart,o.yEnd)&&my<=max(o.yStart,o.yEnd)){ 
+                        ////for floor test
+                        if(wall=="e"){ 
+                            x=map(mx,o.xStart,o.xEnd,o.xMapStart,o.xMapEnd)
+                            y=map(my,o.yStart,o.yEnd,o.yMapStart,o.yMapEnd)
+                            //x=lerp(x, map(mx,o.xStart,o.xEnd,o.xMapStart,o.xMapEnd) ,0.03)
+                            //y=lerp(y, map(my,o.yStart,o.yEnd,o.yMapStart,o.yMapEnd) ,0.03)	
+                            xArray.push({"x":x,"y":y,"id":id})   
+                            //console.log("xxx:"+x+","+y)                         
+                        }
+                        else{
+                            if(idLock<0||idLock==id){
+                                x=lerp(x, map(mx,o.xStart,o.xEnd,o.xMapStart,o.xMapEnd) ,0.3)
+                                y=lerp(y, map(my,o.yStart,o.yEnd,o.yMapStart,o.yMapEnd) ,0.3)	
+                                console.log("id:"+id+","+x+","+y)
+                                idLock=id
+                            }
                         }
                     }
                     else{
                         if(idLock==id) 
                             idLock=-1
-                    } 
+                    }
                 }
             }
     
